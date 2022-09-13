626f3eced03837d06fcb0bbe41f5e6547f79506a
Omar Francisco Martínez Valdez <omar_francisco11@outlook.com>
2022-09-13 12:00:42 -0500

    Inicio vista proveedores

---
+++ php/control_proveedor.php
@@ -0,0 +1,104 @@
+<?php
+//ARCHIVO QUE CONTIENE LA VARIABLE CON LA CONEXION A LA BASE DE DATOS
+include('../php/conexion.php');
+//ARCHIVO QUE CONDICIONA QUE TENGAMOS ACCESO A ESTE ARCHIVO SOLO SI HAY SESSION INICIADA Y NOS PREMITE TIMAR LA INFORMACION DE ESTA
+include('is_logged.php');
+//DEFINIMOS LA ZONA  HORARIA
+date_default_timezone_set('America/Mexico_City');
+$id_user = $_SESSION['user_id'];// ID DEL USUARIO LOGEADO
+$Fecha_hoy = date('Y-m-d');// FECHA ACTUAL
+
+//CON METODO POST TOMAMOS UN VALOR DEL 0 AL 3 PARA VER QUE ACCION HACER (Para Insertar = 0, Consultar = 1, Actualizar = 2, Borar = 3)
+$Accion = $conn->real_escape_string($_POST['accion']);
+
+//UN SWITCH EL CUAL DECIDIRA QUE ACCION REALIZA DEL CRUD (Para Insertar = 0, Consultar = 1, Actualizar = 2, Borar = 3)
+echo "hola aqui estoy";
+switch ($Accion) {
+    case 0:  ///////////////           IMPORTANTE               ///////////////
+        // $Accion es igual a 0 realiza:
+
+        //CON POST RECIBIMOS TODAS LAS VARIABLES DEL FORMULARIO POR EL SCRIPT "add_proveedor.php" QUE NESECITAMOS PARA INSERTAR
+        $nombre = $conn->real_escape_string($_POST['valorNombre']);
+        $direccion = $conn->real_escape_string($_POST['valorDireccion']);
+        $colonia = $conn->real_escape_string($_POST['valorColonia']);
+        $cp = $conn->real_escape_string($_POST['valorCP']);
+        $rfc = $conn->real_escape_string($_POST['valorRFC']);
+        $email = $conn->real_escape_string($_POST['valorEmail']);
+        $telefono = $conn->real_escape_string($_POST['valorTelefono']);
+        $dias_credito = $conn->real_escape_string($_POST['valorDias_Credito']);
+        
+        //VERIFICAMOS QUE NO HALLA UN PROVEEDOR CON LOS MISMOS DATOS
+		if(mysqli_num_rows(mysqli_query($conn, "SELECT * FROM `punto_venta_proveedores` WHERE nombre='$nombre' AND direccion='$direccion' AND colonia='$colonia' AND cp='$cp' AND rfc='$rfc' AND email='$email'"))>0){
+            echo '<script >M.toast({html:"Ya se encuentra un cliente con los mismos datos registrados.", classes: "rounded"})</script>';
+        }else{
+            // SI NO HAY NUNGUNO IGUAL CREAMOS LA SENTECIA SQL  CON LA INFORMACION REQUERIDA Y LA ASIGNAMOS A UNA VARIABLE
+            $sql = "INSERT INTO `punto_venta_proveedores` (nombre, direccion, colonia, cp, rfc, email, telefono, dias_c, usuario, fecha) 
+               VALUES('$nombre', '$direccion', '$colonia', '$cp', '$rfc', '$email', '$telefono', '$dias_credito','$id_user','$Fecha_hoy')";
+            //VERIFICAMOS QUE LA SENTECIA FUE EJECUTADA CON EXITO!
+			if(mysqli_query($conn, $sql)){
+				echo '<script >M.toast({html:"El proveedor se dió de alta satisfactoriamente.", classes: "rounded"})</script>';	
+			}else{
+                echo '<script >M.toast({html:"Ocurrio un error...", classes: "rounded"})</script>';	
+            }//FIN else DE ERROR
+            echo '<script>recargar_proveedores()</script>';// REDIRECCIONAMOS (FUNCION ESTA EN ARCHIVO modals.php)
+        }// FIN else DE BUSCAR PROVEEDOR IGUAL
+
+        break;
+    case 1:
+        // $Accion es igual a 1 realiza:
+        //CON POST RECIBIMOS UN TEXTO DEL BUSCADOR VACIO O NO
+        $Texto = $conn->real_escape_string($_POST['texto']);
+        //VERIFICAMOS SI CONTIENE ALGO DE TEXTO LA VARIABLE
+		if ($Texto != "") {
+			//MOSTRARA LOS CLIENTES QUE SE ESTAN BUSCANDO Y GUARDAMOS LA CONSULTA SQL EN UNA VARIABLE $sql......
+			$sql = "SELECT * FROM `punto_venta_proveedores` WHERE  nombre LIKE '%$Texto%'  OR id = '$Texto' OR rfc = '$Texto' OR colonia LIKE '%$Texto%' OR direccion LIKE '%$Texto%' ORDER BY id";	
+		}else{//ESTA CONSULTA SE HARA SIEMPRE QUE NO ALLA NADA EN EL BUSCADOR Y GUARDAMOS LA CONSULTA SQL EN UNA VARIABLE $sql...
+			$sql = "SELECT * FROM `punto_venta_proveedores`";
+		}//FIN else $Texto VACIO O NO
+
+        // REALIZAMOS LA CONSULTA A LA BASE DE DATOS MYSQL Y GUARDAMOS EN FORMARTO ARRAY EN UNA VARIABLE $consulta
+		$consulta = mysqli_query($conn, $sql);		
+		$contenido = '';//CREAMOS UNA VARIABLE VACIA PARA IR LLENANDO CON LA INFORMACION EN FORMATO
+
+		//VERIFICAMOS QUE LA VARIABLE SI CONTENGA INFORMACION
+		if (mysqli_num_rows($consulta) == 0) {
+				echo '<script>M.toast({html:"No se encontraron proveedores.", classes: "rounded"})</script>';
+        } else {
+            //SI NO ESTA EN == 0 SI TIENE INFORMACION
+            //La variable $resultado contiene el array que se genera en la consulta, así que obtenemos los datos y los mostramos en un bucle
+            //RECORREMOS UNO A UNO LOS CLIENTES CON EL WHILE
+            while($proveedor = mysqli_fetch_array($consulta)) {
+                $proveedor .= '			
+		          <tr>
+		            <td>'.$proveedor['id'].'</td>
+		            <td>'.$proveedor['nombre'].'</td>
+		            <td>'.$proveedor['direccion'].'</td>
+		            <td>'.$proveedor['colonia'].'</td>
+		            <td>'.$proveedor['cp'].'</td>
+		            <td>'.$proveedor['rfc'].'</td>
+		            <td>'.$proveedor['email'].'</td>
+		            <td>'.$proveedor['telefono'].'</td>
+		            <td>'.$proveedor['dias_c'].'</td>
+		            <td>'.$proveedor['usuario'].'</td>
+		            <td>'.$proveedor['fecha'].'</td>
+		            <td><form method="post" action="../views/editar_proveedor.php"><input id="no_proveedor" name="no_proveedor" type="hidden" value="'.$proveedor['id'].'"><button class="btn-floating btn-tiny waves-effect waves-light pink"><i class="material-icons">edit</i></button></form></td>
+		            <td><a onclick="verificar_eliminar('.$proveedor['id'].')" class="btn btn-floating red darken-1 waves-effect waves-light"><i class="material-icons">delete</i></a></td>
+		          </tr>';
+
+			}//FIN while
+        }//FIN else
+
+        echo $contenido;// MOSTRAMOS LA INFORMACION HTML
+
+        break;
+    case 2:
+        // $Accion es igual a 2 realiza:
+        break;
+    case 3:
+        // $Accion es igual a 3 realiza:
+        break;
+
+    }// FIN switch
+    mysqli_close($conn);
+    
+?>
---
+++ views/proveedores_punto_venta.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php
+  //INCLUIMOS EL ARCHIVO QUE CONTIENE LA BARRA DE NAVEGACION TAMBIEN TIENE (scripts, conexion, is_logged, modals)
+  include('fredyNav.php');
+  ?>
+  <title>SIC | Proveedores Punto Venta</title>
+  <script>
+     //FUNCION QUE HACE LA BUSQUEDA DE CLIENTES (SE ACTIVA AL INICIAR EL ARCHIVO O AL ECRIBIR ALGO EN EL BUSCADOR)
+    function buscar_proveedores(){
+      //PRIMERO VAMOS Y BUSCAMOS EN ESTE MISMO ARCHIVO EL TEXTO REQUERIDO Y LO ASIGNAMOS A UNA VARIABLE
+      var texto = $("input#busqueda").val();
+      //MEDIANTE EL METODO POST ENVIAMOS UN ARRAY CON LA INFORMACION AL ARCHIVO EN LA DIRECCION "../php/control_proveedor.php"
+      $.post("../php/control_proveedor.php", {
+        //Cada valor se separa por una ,
+          texto: texto,
+          accion: 1,
+        }, function(mensaje){
+            //SE CREA UNA VARIABLE LA CUAL TRAERA EN TEXTO HTML LOS RESULTADOS QUE ARROJE EL ARCHIVO AL CUAL SE LE ENVIO LA INFORMACION "control_proveedor.php"
+            $("#proveedoresALL").html(mensaje);
+      });
+    }
+  </script>
+</head>
+<main>
+<body onload="buscar_proveedores();">
+  <div class="container"><br><br>
+    <!--    //////    BOTON QUE REDIRECCIONA AL FORMULARIO DE AGREGAR CLIENTE    ///////   -->
+    <a href="add_cliente.php" class="waves-effect waves-light btn pink left right">Agregar Proveedor<i class="material-icons prefix left">add</i></a>
+    <div class="row">
+      <!--    //////    TITULO    ///////   -->
+      <h3 class="hide-on-med-and-down col s12 m6 l6">Proveedores</h3>
+      <h5 class="hide-on-large-only col s12 m6 l6">Proveedores</h5>
+      <!--    //////    INPUT DE EL BUSCADOR    ///////   -->
+      <form class="col s12 m6 l6">
+        <div class="row">
+          <div class="input-field col s12">
+            <i class="material-icons prefix">search</i>
+            <input id="busqueda" name="busqueda" type="text" class="validate" onkeyup="buscar_proveedores();">
+            <label for="busqueda">Buscar(N° Proveedor, Nombre, ,Dirección, Colonia, C.P., RFC)</label>
+          </div>
+        </div>
+      </form>
+    </div>
+    <!--    //////    TABLA QUE MUESTRA LA INFORMACION DE LOS CLIENTES    ///////   -->
+    <div class="row">
+      <table class="bordered highlight responsive-table">
+        <thead>
+          <tr>
+            <th>N°</th>
+            <th>Nombre</th>
+            <th>Dirección</th>
+            <th>Colonia</th>
+            <th>C.P.</th>
+            <th>RFC</th>
+            <th>E-mail</th>
+            <th>Teléfono</th>
+            <th>Días de crédito</th>
+            <th>Registro</th>
+            <th>Fecha</th>
+            <th>Editar</th>
+            <th>Borrar</th>
+          </tr>
+        </thead>
+        <!-- DENTRO DEL tbody COLOCAMOS id = "proveedoresALL"  PARA QUE EN ESTA PARTE NOS MUESTRE LOS RESULTADOS EN TEXTO HTML DEL SCRIPT EN FUNCION buscar_clientes() -->
+        <tbody id="proveedoresALL">
+        </tbody>
+      </table>
+    </div><br><br>
+  </div>
+</body>
+</main>
+</html>
