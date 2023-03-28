221bd234b4c0a6cc8a50a20654b38e8907e9b002
JaguarMX <omar_francisco11@outlook.com>
2023-03-28 16:51:48 -0600

    Update detalle_proveedor_pv.php

--- views/detalle_proveedor_pv.php
+++ views/detalle_proveedor_pv.php
@@ -1,5 +1,5 @@
 <?php
-//VERIFICAMOS QUE SI NOS ENVIE POR POST EL ID DEL USUARIO
+//VERIFICAMOS QUE SI NOS ENVIE POR POST EL ID DEL PROVEEDOR DESDE EL ARCHIVO control_proveedor.php
 if (isset($_POST['proveedor']) == false) {
   ?>
   <script>    
@@ -15,7 +15,7 @@ if (isset($_POST['proveedor']) == false) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hola Gobernador</title>
+    <title>Detalles proveedor</title>
     <?php
       //INCLUIMOS EL ARCHIVO QUE CONTIENE LA BARRA DE NAVEGACION TAMBIEN TIENE (scripts, conexion, is_logged, modals)
       include('fredyNav.php');
